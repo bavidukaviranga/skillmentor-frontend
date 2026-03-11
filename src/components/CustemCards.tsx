25698f0d213529f3a840fcd1570a9084b9847079
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2026-03-11 11:36:37 +0530

    Update CustemCards.tsx

--- src/components/CustemCards.tsx
+++ src/components/CustemCards.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import type { CardElement } from "../types";
+import type { CardElement } from "@/types";
 import { Button } from "./ui/button";
 import {
   Card,
