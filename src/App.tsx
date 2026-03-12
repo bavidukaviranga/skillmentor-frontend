9a595e700de593fc4b4406195b51a5d828e1d3da
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2026-03-13 00:43:37 +0530

    feat: introduce react router concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+import Layout from "./Layout";
+import LoginPage from "./pages/LoginPage";
+import SessionsPage from "./pages/SessionsPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
-  <section>
-      <h1>
-        Hello World
-      </h1>
-  </section>
-  ;
+  return (
+    <Routes>
+      {/* Route groups */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sessions" element={<SessionsPage />} />
+        <Route
+          path="/sessions/:sessionId/details/:sessionTitle"
+          element={<SessionPage />}
+        />
+      </Route>
+
+      {/* 404 page */}
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
