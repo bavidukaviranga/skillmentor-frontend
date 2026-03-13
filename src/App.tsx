dee8536d3dd4634652f6494c1bb2fbb45029d219
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2026-03-13 09:55:18 +0530

    chore: setup clerk

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import Layout from "./Layout";
-import LoginPage from "./pages/LoginPage";
-import SessionsPage from "./pages/SessionsPage";
-import SessionPage from "./pages/SessionPage";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 
 function App() {
   return (
@@ -12,12 +10,18 @@ function App() {
       {/* Route groups */}
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/sessions" element={<SessionsPage />} />
         <Route
-          path="/sessions/:sessionId/details/:sessionTitle"
-          element={<SessionPage />}
+          path="/dashboard"
+          element={
+            <>
+              <SignedIn>
+                <DashboardPage />
+              </SignedIn>
+              <SignedOut>
+                <h1>Please sign in to view the dashboard</h1>
+              </SignedOut>
+            </>
+          }
         />
       </Route>
 
